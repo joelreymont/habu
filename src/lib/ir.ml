0d5e20d3b3a24a3b7263333b00fe37e921b100d0
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2025-06-06 11:59:04 +0300

    Lift the rest of the AST

--- src/lib/ir.ml
+++ src/lib/ir.ml
@@ -43,7 +43,9 @@ module Register = struct
 end
 
 module Register_map = struct
-  type t = Register.t array
+  type t = Register.t option array
+
+  let make ~arr : t = arr
 end
 
 module Bit_field = struct
@@ -62,6 +64,10 @@ module Register_index = struct
     ; map_index : int
     ; bit_field : Bit_field.t
     }
+
+  let make ~id ~map ~index ~field =
+    { id; map; map_index = index; bit_field = field }
+  ;;
 end
 
 module Variable = struct
@@ -79,8 +85,15 @@ module Term = struct
   type _ t =
     | Memory_segment : Memory_segment.t -> Memory_segment.t t
     | Register : Register.t -> Register.t t
-    | Register_index : Register_index.t t
-    | Bit_field : Bit_field.t t
+    | Register_index : Register_index.t -> Register_index.t t
+    | Bit_field : Bit_field.t -> Bit_field.t t
+
+  let value : type a. a t -> a = function
+    | Memory_segment x -> x
+    | Register x -> x
+    | Register_index x -> x
+    | Bit_field x -> x
+  ;;
 end
 
 type term' = Term : 'a Term.t -> term' [@@unboxed]
@@ -160,6 +173,12 @@ module Macro = struct
     ; args : id list
     ; statements : Statement.t list
     }
+
+  let make ~id ~args ~statements = { id; args; statements }
+end
+
+module Pcode_op = struct
+  type t = string
 end
 
 module Type = struct
@@ -169,12 +188,13 @@ module Type = struct
     | Bit_field : Bit_field.t t
     | Macro : Macro.t t
     | Memory_segment : Memory_segment.t t
+    | Pcode_op : Pcode_op.t t
 end
 
 module Type_env = struct
   type t =
     | Nil
-    | Cons : string * 'a Type.t * 'a Term.t * t -> t
+    | Cons : string * 'a Type.t * 'a * t -> t
 
   type ('a, 'b) eq = Refl : ('a, 'a) eq
 
@@ -192,19 +212,19 @@ module Type_env = struct
     | _, _ -> None
   ;;
 
-  let append ~env ~name ~ty ~term = Cons (name, ty, term, env)
+  let append ~env ~name ~ty ~value = Cons (name, ty, value, env)
 
-  let rec lookup_exn : type a. t -> string -> a Type.t -> a Term.t =
+  let rec lookup : type a. t -> string -> a Type.t -> a option =
     fun env name ty ->
     match env with
-    | Nil -> raise Not_found
-    | Cons (xname, xty, term, rest) ->
+    | Nil -> None
+    | Cons (xname, xty, value, rest) ->
       if name = xname then (
         match types_equal ty xty with
-        | Some Refl -> term
+        | Some Refl -> Some value
         | None -> assert false
       ) else
-        lookup_exn rest name ty
+        lookup rest name ty
   ;;
 end
 
@@ -246,7 +266,90 @@ let dummy_id () =
   with_pos dummy ""
 ;;
 
-let lift_exn defs =
+let lift_varnode_exn env (v : S.Varnode.t) =
+  let f id =
+    let name = id.value in
+    (match Type_env.lookup !env name Type.Register with
+     | None ->
+       let pos = position id in
+       Error.error pos (Printf.sprintf "Duplicate register '%s'" name)
+     | _ -> ());
+    let ty = Type.Register
+    and value = Register.make ~id ~size:v.size in
+    env := Type_env.append ~env:!env ~name ~ty ~value
+  in
+  List.iter f v.registers
+;;
+
+let lift_space_exn env (s : S.Space.t) =
+  let name = s.id.value
+  and ty = Type.Memory_segment
+  and value = Memory_segment.lift s in
+  env := Type_env.append ~env:!env ~name ~ty ~value
+;;
+
+let make_register_map_exn env registers =
+  let f id =
+    let name = id.value in
+    if name == "_" then
+      None
+    else (
+      match Type_env.lookup !env name Type.Register with
+      | None ->
+        let pos = position id in
+        Error.error pos (Printf.sprintf "Unknown register '%s'" name)
+      | x -> x
+    )
+  in
+  let arr = Array.of_list (List.map f registers) in
+  Register_map.make ~arr
+;;
+
+let lift_varnode_attach_exn env (vna : S.Varnode_attach.t) =
+  let map = make_register_map_exn env vna.registers in
+  let f index id =
+    let name = id.value in
+    let field =
+      match Type_env.lookup !env name Type.Bit_field with
+      | None ->
+        let pos = position id in
+        Error.error pos (Printf.sprintf "Unknown bit field '%s'" name)
+      | Some value -> value
+    in
+    let ty = Type.Register_index
+    and value = Register_index.make ~id ~map ~index ~field in
+    env := Type_env.append ~env:!env ~name ~ty ~value
+  in
+  List.iteri f vna.fields
+;;
+
+let lift_pcode_op_exn env id =
+  let name = id.value in
+  match Type_env.lookup !env name Type.Pcode_op with
+  | Some _ ->
+    let pos = position id in
+    Error.error pos (Printf.sprintf "Duplicate pcode op '%s'" name)
+  | None ->
+    let ty = Type.Pcode_op
+    and value : Pcode_op.t = name in
+    env := Type_env.append ~env:!env ~name ~ty ~value
+;;
+
+let lift_macro_exn env (m : S.Macro.t) =
+  let name = m.id.value in
+  match Type_env.lookup !env name Type.Pcode_op with
+  | Some _ ->
+    let pos = position m.id in
+    Error.error pos (Printf.sprintf "Duplicate macro '%s'" name)
+  | None ->
+    (* TODO: Check for duplicate arg names? *)
+    let statements = List.map lift_statement_exn m.body in
+    let ty = Type.Macro
+    and value = Macro.make ~id:m.id ~args:m.args ~statements in
+    env := Type_env.append ~env:!env ~name ~ty ~value
+;;
+
+let lift_defs_exn defs =
   let env = ref (Type_env.make ())
   and endian = ref None
   and alignment = ref None
@@ -256,21 +359,11 @@ let lift_exn defs =
   let f = function
     | D.Endian e -> endian := Some e.value
     | D.Alignment a -> alignment := Some a.value
-    | D.Space s ->
-      let name = s.id.value
-      and ty = Type.Memory_segment
-      and segment = Memory_segment.lift s in
-      let term = Term.Memory_segment segment in
-      env := Type_env.append ~env:!env ~name ~ty ~term
-    | D.Varnode v ->
-      let f id =
-        let name = id.value
-        and ty = Type.Register
-        and reg = Register.make ~id ~size:v.size in
-        let term = Term.Register reg in
-        env := Type_env.append ~env:!env ~name ~ty ~term
-      in
-      List.iter f v.registers
+    | D.Space s -> lift_space_exn env s
+    | D.Varnode vn -> lift_varnode_exn env vn
+    | D.Varnode_attach vna -> lift_varnode_attach_exn env vna
+    | D.Pcode_op op -> lift_pcode_op_exn env op
+    | D.Macro m -> lift_macro_exn env m
     | _ -> assert false
   in
   List.iter f defs;
