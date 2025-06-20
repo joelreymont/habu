921b56d9c40cf9c347d5020a45aaef42242983c0
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2025-06-20 14:12:18 +0300

    Simplify testing

--- test/sleigh_parser_test.ml
+++ test/sleigh_parser_test.ml
@@ -3,11 +3,15 @@ open Sexplib.Conv
 open Habu
 open Sleigh_tree
 
+let parse f s =
+  let lexbuf = Lexing.from_string s in
+  let res = Driver.parse s lexbuf f in
+  Result.to_option res
+;;
+
 let%expect_test "parse endian" =
   let text = "define endian = big;" in
-  let lexbuf = Lexing.from_string text in
-  let res = Driver.parse text lexbuf Sleigh_parser.endian_definition in
-  let def = Result.to_option res in
+  let def = parse Sleigh_parser.endian_definition text in
   sexp_of_option Definition.sexp_of_t def |> Sexp.to_string |> print_endline;
   [%expect {| ((Endian((value Big)(tag())))) |}]
 ;;
