92ed44e3c79fff7eeabb92a6b2159370842f89b4
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2025-11-21 12:24:58 +0200

    Add ARM64 opt-in JIT cons/car/cdr test using jit-eval

--- tests/jit-cons-car-cdr.lisp
+++ tests/jit-cons-car-cdr.lisp
@@ -5,16 +5,14 @@
       (format t "[PASS] ~A -> ~A~%" label got)
       (error "[FAIL] ~A got ~A expected ~A" label got expected)))
 
-(defun untag-fixnum (v) (/ v 16))
-
 (defun test-cons-car-cdr ()
   (habu-sbcl:ensure-runtime-addrs)
   (let* ((cons-result (habu-sbcl:jit-eval '(cons 1 2)))
-         (car-result (untag-fixnum (habu-sbcl:jit-eval '(car (cons 1 2)))))
-         (cdr-result (untag-fixnum (habu-sbcl:jit-eval '(cdr (cons 1 2))))))
+         (car-result (habu-sbcl:jit-eval '(car (cons 1 2))))
+         (cdr-result (habu-sbcl:jit-eval '(cdr (cons 1 2)))))
     (expect-eq car-result 1 "car(cons 1 2)")
     (expect-eq cdr-result 2 "cdr(cons 1 2)")
-    (format t "[INFO] cons result raw (tagged): ~A~%" cons-result)))
+    (format t "[INFO] cons result (tagged) maybe non-fixnum: ~A~%" cons-result)))
 
 (let ((jit-allowed (string= (or (sb-posix:getenv "HABU_JIT_TEST") "") "1")))
   (cond
