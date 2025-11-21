a82298475e276879abe7ad3e2b1ed895da6385ef
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2025-11-21 13:39:54 +0200

    Normalize parens in habu-arm64-codegen (real codegen still SBCL-incompatible flag off)

--- habu-arm64-codegen.lisp
+++ habu-arm64-codegen.lisp
@@ -1252,32 +1252,4 @@
 ;;; (compile-to-arm64 42)
 ;;; (compile-to-arm64 (quote (+ 5 7)))
 
-))))))))))))))))
-
-#+sbcl
-(progn
-  (defun codegen-expr (ir runtime-addrs)
-    "SBCL shim: simplified codegen to allow loading; returns move of literal/var or zero."
-    (cond
-      ((has-tag? ir 'lit)
-       (let ((value (cadr ir)))
-         (arm64-movz 0 (* value 16))))
-      ((has-tag? ir 'var)
-       (let ((offset (cadr ir)))
-         (arm64-ldr 0 31 (* offset 16))))
-      (t (arm64-movz 0 #x0))))
-
-  (defun compile-expr (expr env fenv)
-    "SBCL shim: return trivial IR for literals/vars; else zero."
-    (cond
-      ((fixnum? expr) (list 'lit expr))
-      ((symbol? expr)
-       (let ((off (env-lookup expr env)))
-         (if off (list 'var off) (list 'lit 0))))
-      (t (list 'lit 0))))
-
-  (defun compile-to-arm64-with-runtime (expr runtime-addrs)
-    (codegen-main-with-runtime (compile-expr expr nil nil) runtime-addrs))
-
-  (defun compile-to-arm64 (expr)
-    (compile-to-arm64-with-runtime expr *runtime-addrs*))))
+))))))))))))))))))
