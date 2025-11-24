648bc0779098e783b8133a6ff480ee0a2cead301
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2025-11-24 19:54:25 +0200

    Track package symbol-name test and expect tagged string

---
+++ tests/test_symbol_name_package.lisp
@@ -0,0 +1,13 @@
+#!/usr/bin/env sbcl --script
+;;; symbol-name should return a tagged string pointer for quoted symbol.
+(load "run-habu.lisp")
+
+(let* ((forms '((defun sym-name () (symbol-name (quote foo))) (sym-name)))
+       (tag #x4))
+  (multiple-value-bind (result output)
+      (habu-sbcl:compile-and-run-forms forms)
+    (if (and result (= (logand result #xF) tag))
+        (sb-ext:quit :unix-status 0)
+        (progn
+          (format t "Unexpected result ~A Output=~A~%" result output)
+          (sb-ext:quit :unix-status 1)))))
