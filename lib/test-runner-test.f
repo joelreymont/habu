10df9e370c31b947bb0917e9ef97c9b5563be138
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2026-06-20 22:56:45 +0300

    Add native gate runner foundation

---
+++ lib/test-runner-test.f
@@ -0,0 +1,143 @@
+\ test-runner-test.f - focused tests for lib/test-runner.f.
+\ Run: bin/hb --load lib/errors.f lib/string.f lib/test.f lib/fs.f
+\ lib/fs-mutate.f lib/process.f lib/process-argv.f lib/test-runner.f
+\ lib/test-runner-test.f
+
+create GTT-OK-PATH FS-PATH-CAP allot
+create GTT-FAIL-PATH FS-PATH-CAP allot
+create GTT-HANG-PATH FS-PATH-CAP allot
+
+variable GTT-OK-U
+variable GTT-FAIL-U
+variable GTT-HANG-U
+
+: GTT-OK$ ( -- ptr u8 n )
+   GTT-OK-PATH GTT-OK-U @ ;
+
+: GTT-FAIL$ ( -- ptr u8 n )
+   GTT-FAIL-PATH GTT-FAIL-U @ ;
+
+: GTT-HANG$ ( -- ptr u8 n )
+   GTT-HANG-PATH GTT-HANG-U @ ;
+
+: GTT-LF ( -- )
+   10 SB-APPEND-C ;
+
+: GTT-OK-SRC$ ( -- ptr u8 n )
+   SB-RESET
+   s" 97 emit 108 emit 112 emit 104 emit 97 emit 32 emit" SB-APPEND GTT-LF
+   s" 98 emit 101 emit 116 emit 97 emit 10 emit" SB-APPEND GTT-LF
+   SB$ ;
+
+: GTT-FAIL-SRC$ ( -- ptr u8 n )
+   SB-RESET
+   s" 98 emit 97 emit 100 emit 45 emit 111 emit 117 emit 116 emit 10 emit" SB-APPEND GTT-LF
+   s" create E 98 c, 97 c, 100 c, 45 c, 101 c, 114 c, 114 c, 10 c," SB-APPEND GTT-LF
+   s" 2 E 8 write drop" SB-APPEND GTT-LF
+   s" 0 0 7 die" SB-APPEND GTT-LF
+   SB$ ;
+
+: GTT-HANG-SRC$ ( -- ptr u8 n )
+   s" : HANG ( -- ) begin again ; HANG" ;
+
+: GTT-OK-OUT$ ( -- ptr u8 n )
+   SB-RESET
+   s" alpha beta" SB-APPEND GTT-LF
+   SB$ ;
+
+: GTT-FAIL-OUT$ ( -- ptr u8 n )
+   SB-RESET
+   s" bad-out" SB-APPEND GTT-LF
+   SB$ ;
+
+: GTT-FAIL-ERR$ ( -- ptr u8 n )
+   SB-RESET
+   s" bad-err" SB-APPEND GTT-LF
+   SB$ ;
+
+: GTT-PATHS! ( -- )
+   s" ok.f" GTT-OK-PATH GT-PATH GTT-OK-U !
+   s" fail.f" GTT-FAIL-PATH GT-PATH GTT-FAIL-U !
+   s" hang.f" GTT-HANG-PATH GT-PATH GTT-HANG-U ! ;
+
+: GTT-WRITE ( ptr u8 n ptr u8 n -- ) {: path:ptr pathu src:ptr srcu :}
+   path pathu src srcu WRITE-ALL ;
+
+: GTT-PREPARE ( -- )
+   s" habu-test-runner" GT-START
+   GTT-PATHS!
+   GTT-OK$ GTT-OK-SRC$ GTT-WRITE
+   GTT-FAIL$ GTT-FAIL-SRC$ GTT-WRITE
+   GTT-HANG$ GTT-HANG-SRC$ GTT-WRITE ;
+
+: GTT-RUN-HB ( ptr u8 n n -- ) {: script:ptr scriptu timeout :}
+   PROC-ARGV-RESET
+   script scriptu PROC-ARGV+
+   s" bin/hb" timeout GT-RUN ;
+
+: GTT-RUN-OK ( -- )
+   GTT-OK$ 1000 GTT-RUN-HB ;
+
+: GTT-RUN-FAIL ( -- )
+   GTT-FAIL$ 1000 GTT-RUN-HB ;
+
+: GTT-RUN-HANG ( -- )
+   GTT-HANG$ 50 GTT-RUN-HB ;
+
+: GTT-TEST-TEMP-ROOT ( -- )
+   GT-ROOT DIR? TTRUE
+   GTT-OK$ FILE? TTRUE
+   GTT-FAIL$ FILE? TTRUE
+   GTT-HANG$ FILE? TTRUE ;
+
+: GTT-TEST-PASSING-COMMAND ( -- )
+   GTT-RUN-OK
+   0 s" ok rc" GT-RC=
+   GTT-OK-OUT$ s" ok stdout exact" GT-STDOUT=
+   s" alpha" s" ok stdout contains alpha" GT-STDOUT-HAS
+   s" beta" s" ok stdout contains beta" GT-STDOUT-HAS
+   s" " s" ok stderr empty" GT-STDERR=
+   GT-FAILURES 0 T= ;
+
+: GTT-TEST-FAILING-COMMAND ( -- )
+   GTT-RUN-FAIL
+   7 s" fail rc exact" GT-RC=
+   s" fail rc nonzero" GT-RC-NONZERO
+   GTT-FAIL-OUT$ s" fail stdout exact" GT-STDOUT=
+   s" bad-out" s" fail stdout contains" GT-STDOUT-HAS
+   GTT-FAIL-ERR$ s" fail stderr exact" GT-STDERR=
+   s" bad-err" s" fail stderr contains" GT-STDERR-HAS
+   GT-FAILURES 0 T= ;
+
+: GTT-TEST-TIMEOUT ( -- )
+   GTT-RUN-HANG
+   s" hang timeout" GT-TIMEOUT
+   s" hang nonzero" GT-RC-NONZERO
+   GT-FAILURES 0 T= ;
+
+: GTT-TEST-AGGREGATE-FAILURES ( -- )
+   GTT-RUN-OK
+   1 s" deliberate rc mismatch" GT-RC=
+   GT-FAILURES 1 T=
+   0 GT-FAIL-NAME$ s" deliberate rc mismatch" T$=
+   s" deliberate mismatch" GT-FAIL+
+   GT-FAILURES 2 T=
+   1 GT-FAIL-NAME$ s" deliberate mismatch" T$=
+   GT-RESET
+   GT-FAILURES 0 T= ;
+
+: TEST-RUNNER-TEST-MAIN ( -- )
+   T-RESET
+   GTT-PREPARE
+   GTT-TEST-TEMP-ROOT
+   GTT-TEST-PASSING-COMMAND
+   GTT-TEST-FAILING-COMMAND
+   GTT-TEST-TIMEOUT
+   GTT-TEST-AGGREGATE-FAILURES
+   GT-CLEANUP
+   GT-ROOT EXISTS? TFALSE
+   T-REPORT
+   GT-REPORT
+   s" test-runner-test: ok" type cr ;
+
+TEST-RUNNER-TEST-MAIN
---
+++ lib/test-runner.f
@@ -0,0 +1,145 @@
+\ test-runner.f - checked native gate runner foundation.
+\
+\ Load after lib/errors.f, lib/string.f, lib/fs.f, lib/fs-mutate.f,
+\ lib/process.f, and lib/process-argv.f.
+
+2 constant GT-EX-FAIL
+64 constant GT-FAIL-MAX
+128 constant GT-FAIL-NAME-CAP
+32768 constant GT-OUT-CAP
+32768 constant GT-ERR-CAP
+10000 constant GT-DEFAULT-TIMEOUT-MS
+
+create GT-ROOT-BUF FS-PATH-CAP allot
+create GT-OUT-BUF GT-OUT-CAP allot
+create GT-ERR-BUF GT-ERR-CAP allot
+create GT-FAIL-NAMES GT-FAIL-MAX GT-FAIL-NAME-CAP * allot
+create GT-FAIL-US GT-FAIL-MAX cells allot
+
+variable GT-ROOT-U
+variable GT-OUT-U
+variable GT-ERR-U
+variable GT-OUTCOME-KIND
+variable GT-OUTCOME-CODE
+variable GT-FAIL#
+
+: GT-FAIL-SLOT ( n -- ptr u8 ) {: idx :}
+   idx 0 < if E-TBL-BOUNDS throw then
+   idx GT-FAIL-MAX >= if E-TBL-BOUNDS throw then
+   idx GT-FAIL-NAME-CAP * GT-FAIL-NAMES + ;
+
+: GT-FAIL-U-PTR ( n -- ptr n ) {: idx :}
+   idx 0 < if E-TBL-BOUNDS throw then
+   idx GT-FAIL-MAX >= if E-TBL-BOUNDS throw then
+   idx cells GT-FAIL-US + ;
+
+: GT-FAIL-NAME$ ( n -- ptr u8 n ) {: idx :}
+   idx GT-FAIL-SLOT
+   idx GT-FAIL-U-PTR @ ;
+
+: GT-RESET ( -- )
+   0 GT-OUT-U !
+   0 GT-ERR-U !
+   PROC-OUTCOME-EXIT GT-OUTCOME-KIND !
+   0 GT-OUTCOME-CODE !
+   0 GT-FAIL# ! ;
+
+: GT-ROOT ( -- ptr u8 n )
+   GT-ROOT-BUF GT-ROOT-U @ ;
+
+: GT-OUT$ ( -- ptr u8 n )
+   GT-OUT-BUF GT-OUT-U @ ;
+
+: GT-ERR$ ( -- ptr u8 n )
+   GT-ERR-BUF GT-ERR-U @ ;
+
+: GT-FAILURES ( -- n )
+   GT-FAIL# @ ;
+
+: GT-EXPECT-ROOT ( -- )
+   GT-ROOT-U @ 0 <= if E-FS-PATH throw then ;
+
+: GT-COPY-ROOT! ( ptr u8 n -- ) {: a:ptr u :}
+   u 0 < if E-FS-PATH throw then
+   u FS-PATH-CAP > if E-FS-PATH throw then
+   a GT-ROOT-BUF u BYTE-COPY
+   u GT-ROOT-U ! ;
+
+: GT-START ( ptr u8 n -- ) {: prefix:ptr prefixu :}
+   GT-RESET
+   CLEANUP-RESET
+   0 GT-ROOT-U !
+   prefix prefixu TMPDIR-MKDIR GT-COPY-ROOT!
+   GT-ROOT CLEANUP-TREE+ ;
+
+: GT-CLEANUP ( -- )
+   CLEANUP-RUN ;
+
+: GT-PATH ( ptr u8 n ptr u8 -- n ) {: name:ptr nameu dst:ptr :}
+   GT-EXPECT-ROOT
+   GT-ROOT name nameu dst JOIN-PATH ;
+
+: GT-FAIL+ ( ptr u8 n -- ) {: name:ptr nameu :}
+   GT-FAIL# @ GT-FAIL-MAX >= if E-TBL-BOUNDS throw then
+   nameu 0 < if E-TBL-FIELD throw then
+   nameu GT-FAIL-NAME-CAP > if E-TBL-FIELD throw then
+   name GT-FAIL# @ GT-FAIL-SLOT nameu BYTE-COPY
+   nameu GT-FAIL# @ GT-FAIL-U-PTR !
+   GT-FAIL# @ 1+ GT-FAIL# ! ;
+
+: GT-CHECK ( bool ptr u8 n -- ) {: ok name:ptr nameu :}
+   ok 0= if name nameu GT-FAIL+ then ;
+
+: GT-RUN ( ptr u8 n n -- ) {: path:ptr pathu timeout :}
+   path pathu GT-OUT-BUF GT-OUT-CAP GT-ERR-BUF GT-ERR-CAP timeout
+   RUN-ARGV-CAPTURE-OUTCOME
+   GT-OUTCOME-CODE !
+   GT-OUTCOME-KIND !
+   GT-ERR-U !
+   GT-OUT-U ! ;
+
+: GT-RUN-DEFAULT ( ptr u8 n -- )
+   GT-DEFAULT-TIMEOUT-MS GT-RUN ;
+
+: GT-RC@ ( -- n )
+   GT-OUTCOME-KIND @ PROC-OUTCOME-EXIT = if
+      GT-OUTCOME-CODE @ exit
+   then
+   128 GT-OUTCOME-CODE @ + ;
+
+: GT-RC= ( n ptr u8 n -- ) {: want name:ptr nameu :}
+   GT-RC@ want = name nameu GT-CHECK ;
+
+: GT-RC-NONZERO ( ptr u8 n -- ) {: name:ptr nameu :}
+   GT-RC@ 0 <> name nameu GT-CHECK ;
+
+: GT-TIMEOUT ( ptr u8 n -- ) {: name:ptr nameu :}
+   GT-OUTCOME-KIND @ PROC-OUTCOME-TIMEOUT = name nameu GT-CHECK ;
+
+: GT-STDOUT= ( ptr u8 n ptr u8 n -- ) {: want:ptr wantu name:ptr nameu :}
+   GT-OUT$ want wantu STR= name nameu GT-CHECK ;
+
+: GT-STDERR= ( ptr u8 n ptr u8 n -- ) {: want:ptr wantu name:ptr nameu :}
+   GT-ERR$ want wantu STR= name nameu GT-CHECK ;
+
+: GT-STDOUT-HAS ( ptr u8 n ptr u8 n -- ) {: needle:ptr needleu name:ptr nameu :}
+   GT-OUT$ needle needleu CONTAINS? name nameu GT-CHECK ;
+
+: GT-STDERR-HAS ( ptr u8 n ptr u8 n -- ) {: needle:ptr needleu name:ptr nameu :}
+   GT-ERR$ needle needleu CONTAINS? name nameu GT-CHECK ;
+
+: GT-REPORT-FAILS ( -- )
+   0 begin dup GT-FAIL# @ < while
+      s" FAIL: " type
+      dup GT-FAIL-NAME$ type cr
+      1+
+   repeat drop ;
+
+: GT-REPORT ( -- )
+   GT-FAIL# @ 0= if
+      s" test-runner: ok" type cr
+      exit
+   then
+   s" test-runner: " type GT-FAIL# @ . s" failure(s)" type cr
+   GT-REPORT-FAILS
+   s" test-runner: failures" GT-EX-FAIL die ;
