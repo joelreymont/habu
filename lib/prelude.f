9991f150fa584bbcd1e2d01acc77b8e5fb9dabec
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2026-06-27 14:15:18 +0200

    Export prelude signatures

--- lib/prelude.f
+++ lib/prelude.f
@@ -4,6 +4,14 @@
 \ Note: f<= / f>= (names with <= / >=) and fdup / fover (whose dup / over bodies
 \ infer a generic cell, not r) are omitted for now; tracked as a follow-up dot.
 
+: export ( -- )
+   parse-name 2drop ;
+
+export true
+export false
+export 0<>
+export fdrop
+
 : true  ( -- bool ) 0 0= ;
 : false ( -- bool ) 0 0= 0= ;
 : 0<>   ( n -- bool ) 0 <> ;
