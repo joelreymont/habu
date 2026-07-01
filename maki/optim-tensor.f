b09293aad6fd3d3e3da029b04b45744fead592b9
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2026-07-01 19:41:10 +0200

    Remove stale mechanical line effects

--- maki/optim-tensor.f
+++ maki/optim-tensor.f
@@ -8,20 +8,20 @@ require maki/optim.f
 
 package MAKI
 
-: TT-ADAM-AT! ( r r r r r r ptr a ptr a ptr a ptr a n -- ) \ ( in -- )
-   {: lr:r b1:r b2:r eps:r c1:r c2:r wp:ptr gp:ptr mp:ptr vp:ptr idx:n :} \ ( in -- )
-   wp idx T-GET  gp idx T-GET  mp idx T-GET  vp idx T-GET \ ( -- w g m v )
-   lr b1 b2 eps c1 c2 ADAM \ ( w g m v -- w' m' v' )
-   vp idx T-SET \ ( w' m' v' -- w' m' )
-   mp idx T-SET \ ( w' m' -- w' )
-   wp idx T-SET ; \ ( w' -- )
+: TT-ADAM-AT! ( r r r r r r ptr a ptr a ptr a ptr a n -- )
+   {: lr:r b1:r b2:r eps:r c1:r c2:r wp:ptr gp:ptr mp:ptr vp:ptr idx:n :}
+   wp idx T-GET  gp idx T-GET  mp idx T-GET  vp idx T-GET
+   lr b1 b2 eps c1 c2 ADAM
+   vp idx T-SET
+   mp idx T-SET
+   wp idx T-SET ;
 
 public
 
-: TT-ADAM! ( r r r r r r ptr a ptr a ptr a ptr a n -- ) \ ( in -- )
-   {: lr:r b1:r b2:r eps:r c1:r c2:r wp:ptr gp:ptr mp:ptr vp:ptr len:n :} \ ( in -- )
-   len 0 ?do \ ( -- )
-      lr b1 b2 eps c1 c2 wp gp mp vp i TT-ADAM-AT! \ ( -- )
-   loop ; \ ( -- )
+: TT-ADAM! ( r r r r r r ptr a ptr a ptr a ptr a n -- )
+   {: lr:r b1:r b2:r eps:r c1:r c2:r wp:ptr gp:ptr mp:ptr vp:ptr len:n :}
+   len 0 ?do
+      lr b1 b2 eps c1 c2 wp gp mp vp i TT-ADAM-AT!
+   loop ;
 
 end-package
