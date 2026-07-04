9acbc141b44df0bfe59a2198683ccd886b38903a
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2026-07-04 21:58:55 +0200

    PTX: migrate cuda-launch off raw DLSYM

--- tools/ptx/cuda-launch.f
+++ tools/ptx/cuda-launch.f
@@ -16,49 +16,45 @@ require lib/fmt.f
 require src/arch/ptx/emit.f
 require lib/ptx/cg.f
 require lib/ffi.f
+require lib/ptx/cuda-driver.f
 require lib/test.f
 
-create LL-LIB  16 allot
-create LL-NM   64 allot          \ symbol-name scratch
 create LL-PATH 64 allot
-create LL-KN   32 allot          \ kernel name (separate from LL-NM)
-variable LL-H  variable LL-DEV  variable LL-CTX  variable LL-MOD  variable LL-FUNC
+create LL-KN   32 allot          \ kernel name
+variable LL-DEV  variable LL-CTX  variable LL-MOD  variable LL-FUNC
 variable LL-DX variable LL-DY variable LL-ABITS variable LL-NV variable LL-RBUF
 
-: LL-OPEN ( -- )  s" libcuda.so.1" LL-LIB >CSTR  LL-LIB RTLD-NOW DLOPEN LL-H ! ;
-: LL-SYM ( ptr u8 n -- n )  LL-NM >CSTR  LL-H @ LL-NM DLSYM ;
-
 : LL-SETUP ( -- )                                 \ ctx + module + function
-   0                       s" cuInit"                   LL-SYM CALL1 drop
-   LL-DEV P>N 0            s" cuDeviceGet"              LL-SYM CALL2 drop
-   LL-CTX P>N LL-DEV @     s" cuDevicePrimaryCtxRetain" LL-SYM CALL2 drop
-   LL-CTX @               s" cuCtxSetCurrent"          LL-SYM CALL1 drop
+   0 CUDA:CU-INIT CUDA:RC0
+   LL-DEV 0 >IDX CUDA:CU-DEVICE-GET CUDA:RC0
+   LL-CTX LL-DEV @ >CUDA-DEV CUDA:CU-DEVICE-PRIMARY-CTX-RETAIN CUDA:RC0
+   LL-CTX @ >CUDA-CTX CUDA:CU-CTX-SET-CURRENT CUDA:RC0
    s" /tmp/saxpy.cubin" LL-PATH >CSTR
-   LL-MOD P>N LL-PATH P>N s" cuModuleLoad"             LL-SYM CALL2 drop
+   LL-MOD LL-PATH CUDA:CU-MODULE-LOAD CUDA:RC0
    s" SAXPY" LL-KN >CSTR
-   LL-FUNC P>N LL-MOD @ LL-KN P>N s" cuModuleGetFunction" LL-SYM CALL3 drop ;
+   LL-FUNC LL-MOD @ >CUDA-MOD LL-KN CUDA:CU-MODULE-GET-FUNCTION CUDA:RC0 ;
 
 : LL-LAUNCH ( r -- )  {: a:r :}                   \ marshal a, alloc, launch, copy back, free
-   LL-DX P>N 16           s" cuMemAlloc_v2"   LL-SYM CALL2 drop
-   LL-DY P>N 16           s" cuMemAlloc_v2"   LL-SYM CALL2 drop
-   LL-DX @ 2.0 F64>F32 4  s" cuMemsetD32_v2"  LL-SYM CALL3 drop   \ x = 2.0
-   LL-DY @ 0 4            s" cuMemsetD32_v2"  LL-SYM CALL3 drop   \ y = 0
-   a F64>F32 LL-ABITS !  4 LL-NV !                                \ arbitrary a, n = 4
-   LL-FUNC @ 256 1 1      s" cuFuncSetBlockShape" LL-SYM CALL4 drop
-   LL-FUNC @ 24           s" cuParamSetSize"  LL-SYM CALL2 drop
-   LL-FUNC @ 0  LL-DX P>N 8    s" cuParamSetv" LL-SYM CALL4 drop  \ p_x
-   LL-FUNC @ 8  LL-DY P>N 8    s" cuParamSetv" LL-SYM CALL4 drop  \ p_y
-   LL-FUNC @ 16 LL-ABITS P>N 4 s" cuParamSetv" LL-SYM CALL4 drop  \ p_a
-   LL-FUNC @ 20 LL-NV P>N 4    s" cuParamSetv" LL-SYM CALL4 drop  \ p_n
-   LL-FUNC @ 1 1          s" cuLaunchGrid"    LL-SYM CALL3 drop
-   0                      s" cuCtxSynchronize" LL-SYM CALL1 drop
-   LL-RBUF P>N LL-DY @ 4  s" cuMemcpyDtoH_v2" LL-SYM CALL3 drop
-   LL-DX @  s" cuMemFree_v2" LL-SYM CALL1 drop
-   LL-DY @  s" cuMemFree_v2" LL-SYM CALL1 drop ;
+   LL-DX 16 >LEN CUDA:CU-MEM-ALLOC CUDA:RC0
+   LL-DY 16 >LEN CUDA:CU-MEM-ALLOC CUDA:RC0
+   LL-DX @ >CUDA-DEVPTR 2.0 F64>F32 4 >COUNT CUDA:CU-MEMSET-D32 CUDA:RC0   \ x = 2.0
+   LL-DY @ >CUDA-DEVPTR 0 4 >COUNT CUDA:CU-MEMSET-D32 CUDA:RC0             \ y = 0
+   a F64>F32 LL-ABITS !  4 LL-NV !                                        \ arbitrary a, n = 4
+   LL-FUNC @ >CUDA-FN 256 1 1 CUDA:CU-FUNC-SET-BLOCK-SHAPE CUDA:RC0
+   LL-FUNC @ >CUDA-FN 24 >LEN CUDA:CU-PARAM-SET-SIZE CUDA:RC0
+   LL-FUNC @ >CUDA-FN 0 >IDX  LL-DX 8 >LEN CUDA:CU-PARAM-SET-V CUDA:RC0    \ p_x
+   LL-FUNC @ >CUDA-FN 8 >IDX  LL-DY 8 >LEN CUDA:CU-PARAM-SET-V CUDA:RC0    \ p_y
+   LL-FUNC @ >CUDA-FN 16 >IDX LL-ABITS 4 >LEN CUDA:CU-PARAM-SET-V CUDA:RC0 \ p_a
+   LL-FUNC @ >CUDA-FN 20 >IDX LL-NV 4 >LEN CUDA:CU-PARAM-SET-V CUDA:RC0    \ p_n
+   LL-FUNC @ >CUDA-FN 1 1 CUDA:CU-LAUNCH-GRID CUDA:RC0
+   CUDA:CU-CTX-SYNCHRONIZE CUDA:RC0
+   LL-RBUF LL-DY @ >CUDA-DEVPTR 4 >LEN CUDA:DTOH
+   LL-DX @ >CUDA-DEVPTR CUDA:CU-MEM-FREE CUDA:RC0
+   LL-DY @ >CUDA-DEVPTR CUDA:CU-MEM-FREE CUDA:RC0 ;
 
 : LL-RELEASE ( -- )
-   LL-MOD @  s" cuModuleUnload"          LL-SYM CALL1 drop
-   LL-DEV @  s" cuDevicePrimaryCtxRelease" LL-SYM CALL1 drop ;
+   LL-MOD @ >CUDA-MOD CUDA:CU-MODULE-UNLOAD CUDA:RC0
+   LL-DEV @ >CUDA-DEV CUDA:CU-DEVICE-PRIMARY-CTX-RELEASE CUDA:RC0 ;
 
 : SAXPY-GPU-BITS ( -- n )  LL-RBUF @ $FFFFFFFF and ;   \ read-back f32 bits
 
@@ -79,8 +75,7 @@ variable LL-DX variable LL-DY variable LL-ABITS variable LL-NV variable LL-RBUF
 : RUN ( -- )
    T-RESET
    HOST-CHECK
-   LL-OPEN
-   LL-H @ 0= if
+   CUDA:OPEN? 0= if
       s" cuda-launch: libcuda unavailable -> device leg SKIPPED (host marshalling proven)" type cr
       T-REPORT exit
    then
