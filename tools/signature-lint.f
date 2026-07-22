e10dc18699b1148a5b492aa90d17aadd07864c74
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2026-07-22 17:50:53 +0200

    Package signature lint command

--- tools/signature-lint.f
+++ tools/signature-lint.f
@@ -3,23 +3,28 @@
 \ tools/lint/token.f, tools/lint/lib.f, tools/lint/json-writer.f,
 \ tools/lint/source-lex.f, tools/signature-lint-core.f, and tools/argv.f.
 
-: SIGNATURE-LINT-ARGV-FILE ( n -- ) {: idx :}
+package SIGNATURE-LINT-CLI
+private
+
+: ARGV-FILE ( n -- ) {: idx:n :}
    ARGV:LABEL? IF
       idx ARGV:POS$ ARGV:LABEL$ SIGNATURE-LINT-FILE-AS
    ELSE
       idx ARGV:POS$ SIGNATURE-LINT-FILE
    THEN ;
 
-: SIGNATURE-LINT ( -- )
+: RUN ( -- )
    s" tools/signature-lint.f [--json] [--label name] file ..." ARGV:USAGE!
    ARGV:PARSE
    1 -1 ARGV:EXPECT-POS
    SIGNATURE-LINT-RESET
    ARGV:JSON? SL-JSON!
    0 begin dup ARGV:POS# < while
-      dup SIGNATURE-LINT-ARGV-FILE
+      dup ARGV-FILE
       1+
    repeat drop
    SIGNATURE-LINT-FINISH ;
 
-SIGNATURE-LINT
+RUN
+
+;package
