1fe5da0b357ec73c7aec57eff0e3daa96a1d6a88
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2026-08-05 22:11:59 +0200

    Hard-cut MLP tensor pointer types

--- maki/mlp.f
+++ maki/mlp.f
@@ -13,7 +13,7 @@ package MAKI
 public
 
 \ forward: Z1 = X.W1+b1 ; H = relu(Z1) ; Y = H.W2+b2   (Z1,H saved for backward)
-: MLP-FWD ( ptr a ptr a ptr a ptr a ptr a ptr a ptr a ptr a n n n n -- )
+: MLP-FWD ( ptr r ptr r ptr r ptr r ptr r ptr r ptr r ptr r n n n n -- )
    {: xb:ptr w1:ptr b1:ptr w2:ptr b2:ptr z1:ptr hb:ptr yb:ptr
       rows:n in:n hid:n out:n :}
    xb w1 b1 z1  rows in hid  LINEAR
@@ -21,7 +21,7 @@ public
    hb w2 b2 yb  rows hid out  LINEAR ;
 
 \ backward from dY: dH,dW2,dB2 (layer 2) -> dZ1 (relu gate on Z1) -> dX,dW1,dB1
-: MLP-BWD ( ptr a ptr a ptr a ptr a ptr a ptr a ptr a ptr a ptr a ptr a ptr a ptr a ptr a n n n n -- )
+: MLP-BWD ( ptr r ptr r ptr r ptr r ptr r ptr r ptr r ptr r ptr r ptr r ptr r ptr r ptr r n n n n -- )
    {: dyb:ptr xb:ptr w1:ptr z1:ptr hb:ptr w2:ptr
       dhb:ptr dz1:ptr dxb:ptr dw1:ptr db1:ptr dw2:ptr db2:ptr
       rows:n in:n hid:n out:n :}
@@ -30,7 +30,7 @@ public
    dz1 xb w1  dxb dw1 db1  rows in hid  LINEAR-BWD ;
 
 \ one SGD step over all four parameter tensors: p -= lr * dp
-: MLP-SGD ( r ptr a ptr a ptr a ptr a ptr a ptr a ptr a ptr a n n n -- )
+: MLP-SGD ( r ptr r ptr r ptr r ptr r ptr r ptr r ptr r ptr r n n n -- )
    {: lr:r w1:ptr b1:ptr w2:ptr b2:ptr dw1:ptr db1:ptr dw2:ptr db2:ptr
       in:n hid:n out:n :}
    lr w1 dw1  in hid *   T-SGD!
