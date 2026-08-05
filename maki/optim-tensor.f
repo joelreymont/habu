2a8559462a8b5aa5458dbdab22480ba6eb9ce783
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2026-08-05 20:37:59 +0200

    Hard-cut optimizer float buffers

--- maki/optim-tensor.f
+++ maki/optim-tensor.f
@@ -10,7 +10,7 @@ require maki/optim.f
 
 package OPTIM
 
-: TT-ADAM-AT! ( r r r r r r ptr a ptr a ptr a ptr a n -- )
+: TT-ADAM-AT! ( r r r r r r ptr r ptr r ptr r ptr r n -- )
    {: lr:r b1:r b2:r eps:r c1:r c2:r wp:ptr gp:ptr mp:ptr vp:ptr idx:n :}
    wp idx T-GET  gp idx T-GET  mp idx T-GET  vp idx T-GET
    lr b1 b2 eps c1 c2 ADAM
@@ -20,7 +20,7 @@ package OPTIM
 
 \ one AdamW element: decoupled decay wd is applied to the weight alongside the
 \ raw-gradient Adam step (never through m, v) - see OPTIM:ADAMW.
-: TT-ADAMW-AT! ( r r r r r r r ptr a ptr a ptr a ptr a n -- )
+: TT-ADAMW-AT! ( r r r r r r r ptr r ptr r ptr r ptr r n -- )
    {: lr:r b1:r b2:r eps:r c1:r c2:r wd:r wp:ptr gp:ptr mp:ptr vp:ptr idx:n :}
    wp idx T-GET  gp idx T-GET  mp idx T-GET  vp idx T-GET
    lr b1 b2 eps c1 c2 wd ADAMW
@@ -30,7 +30,7 @@ package OPTIM
 
 public
 
-: TT-ADAM! ( r r r r r r ptr a ptr a ptr a ptr a n -- )
+: TT-ADAM! ( r r r r r r ptr r ptr r ptr r ptr r n -- )
    {: lr:r b1:r b2:r eps:r c1:r c2:r wp:ptr gp:ptr mp:ptr vp:ptr len:n :}
    len 0 ?do
       lr b1 b2 eps c1 c2 wp gp mp vp i TT-ADAM-AT!
@@ -39,7 +39,7 @@ public
 \ AdamW over a whole parameter tensor: like TT-ADAM! but each weight also takes
 \ the decoupled decay -lr*wd*w. wd=0 is bit-identical to TT-ADAM!. The wd domain
 \ is guarded once (a negative decay coefficient is meaningless -> throw).
-: TT-ADAMW! ( r r r r r r r ptr a ptr a ptr a ptr a n -- )
+: TT-ADAMW! ( r r r r r r r ptr r ptr r ptr r ptr r n -- )
    {: lr:r b1:r b2:r eps:r c1:r c2:r wd:r wp:ptr gp:ptr mp:ptr vp:ptr len:n :}
    wd 0.0 f< if E-ADAMW-WD throw then
    len 0 ?do
