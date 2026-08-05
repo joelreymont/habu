98e7c76a62d9241ec5947a3c9fa0a6be7a486232
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2026-08-05 21:44:40 +0200

    Hard-cut autograd tensor buffers to real

--- maki/autograd-tensor.f
+++ maki/autograd-tensor.f
@@ -13,50 +13,50 @@ package MAKI
 public
 
 \ --- forward (apply the scalar op elementwise into the output tensor) ---
-: TT-ADD-F  ( ptr a ptr a ptr a n -- ) {: xb:ptr yb:ptr zb:ptr len:n :}
+: TT-ADD-F  ( ptr r ptr r ptr r n -- ) {: xb:ptr yb:ptr zb:ptr len:n :}
    len 0 ?do  xb i T-GET  yb i T-GET  ADD-F  zb i T-SET  loop ;
-: TT-MUL-F  ( ptr a ptr a ptr a n -- ) {: xb:ptr yb:ptr zb:ptr len:n :}
+: TT-MUL-F  ( ptr r ptr r ptr r n -- ) {: xb:ptr yb:ptr zb:ptr len:n :}
    len 0 ?do  xb i T-GET  yb i T-GET  MUL-F  zb i T-SET  loop ;
-: TT-SUB-F  ( ptr a ptr a ptr a n -- ) {: xb:ptr yb:ptr zb:ptr len:n :}
+: TT-SUB-F  ( ptr r ptr r ptr r n -- ) {: xb:ptr yb:ptr zb:ptr len:n :}
    len 0 ?do  xb i T-GET  yb i T-GET  SUB-F  zb i T-SET  loop ;
-: TT-RELU-F ( ptr a ptr a n -- )       {: xb:ptr zb:ptr len:n :}
+: TT-RELU-F ( ptr r ptr r n -- )       {: xb:ptr zb:ptr len:n :}
    len 0 ?do  xb i T-GET  RELU-F   zb i T-SET  loop ;
-: TT-SQUARE-F ( ptr a ptr a n -- )     {: xb:ptr zb:ptr len:n :}
+: TT-SQUARE-F ( ptr r ptr r n -- )     {: xb:ptr zb:ptr len:n :}
    len 0 ?do  xb i T-GET  SQUARE-F  zb i T-SET  loop ;
-: TT-SUM-F  ( ptr a n -- r )  T-SUM ;       \ reduce the whole tensor to a scalar
+: TT-SUM-F  ( ptr r n -- r )  T-SUM ;       \ reduce the whole tensor to a scalar
 
 \ --- backward (map the scalar VJP; write the input-cotangent tensor(s)) ---
 \ ADD: dX=dZ, dY=dZ
-: TT-ADD-BWD ( ptr a ptr a ptr a n -- ) {: dzb:ptr dxb:ptr dyb:ptr len:n :}
+: TT-ADD-BWD ( ptr r ptr r ptr r n -- ) {: dzb:ptr dxb:ptr dyb:ptr len:n :}
    len 0 ?do
       dzb i T-GET  ADD-BWD              \ ( dx dy )
       dyb i T-SET  dxb i T-SET
    loop ;
 \ MUL: dX=dZ*Y, dY=dZ*X  (saved primals X, Y)
-: TT-MUL-BWD ( ptr a ptr a ptr a ptr a ptr a n -- )
+: TT-MUL-BWD ( ptr r ptr r ptr r ptr r ptr r n -- )
    {: dzb:ptr xb:ptr yb:ptr dxb:ptr dyb:ptr len:n :}
    len 0 ?do
       dzb i T-GET  xb i T-GET  yb i T-GET  MUL-BWD   \ ( dx dy )
       dyb i T-SET  dxb i T-SET
    loop ;
 \ SUB: dX=dZ, dY=-dZ
-: TT-SUB-BWD ( ptr a ptr a ptr a n -- ) {: dzb:ptr dxb:ptr dyb:ptr len:n :}
+: TT-SUB-BWD ( ptr r ptr r ptr r n -- ) {: dzb:ptr dxb:ptr dyb:ptr len:n :}
    len 0 ?do
       dzb i T-GET  SUB-BWD              \ ( dx dy )
       dyb i T-SET  dxb i T-SET
    loop ;
 \ RELU: dX = x>0 ? dZ : 0  (saved primal X)
-: TT-RELU-BWD ( ptr a ptr a ptr a n -- ) {: dzb:ptr xb:ptr dxb:ptr len:n :}
+: TT-RELU-BWD ( ptr r ptr r ptr r n -- ) {: dzb:ptr xb:ptr dxb:ptr len:n :}
    len 0 ?do
       dzb i T-GET  xb i T-GET  RELU-BWD  dxb i T-SET
    loop ;
 \ SQUARE: dX = dZ*2X  (saved primal X)
-: TT-SQUARE-BWD ( ptr a ptr a ptr a n -- ) {: dzb:ptr xb:ptr dxb:ptr len:n :}
+: TT-SQUARE-BWD ( ptr r ptr r ptr r n -- ) {: dzb:ptr xb:ptr dxb:ptr len:n :}
    len 0 ?do
       dzb i T-GET  xb i T-GET  SQUARE-BWD  dxb i T-SET
    loop ;
 \ SUM: the scalar output cotangent broadcasts to every input element (dX[i]=dZ)
-: TT-SUM-BWD ( r ptr a n -- ) {: dz:r dxb:ptr len:n :}
+: TT-SUM-BWD ( r ptr r n -- ) {: dz:r dxb:ptr len:n :}
    dz dxb len T-FILL ;
 
 ;package
